ce7034560358520eba6a1b1bc35341b05de70743
S00188509 <S00188509@VLAB-KSODVY2-20>
2020-05-13 10:30:46 +0100

    NUnit Test Created

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlanRegan_S00188509;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+     
+            [TestMethod]
+            public void TestIncreasePrice()
+            {
+                // Arrange
+                Phone phone1 = new Phone();
+                decimal expectedPrice = 875m;
+                phone1.Price = 500m;
+
+                // Act
+                phone1.IncreasePrice(.75m);
+
+                // Assert
+                Assert.AreEqual(expectedPrice, phone1.Price);
+            }
+        }
+}
