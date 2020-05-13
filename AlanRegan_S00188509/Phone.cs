f1fac7dffe8ece2483c7dfed56aa733bb07cbde8
S00188509 <S00188509@VLAB-KSODVY2-20>
2020-05-13 10:19:34 +0100

    Properties & Increase method added

---
+++ AlanRegan_S00188509/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlanRegan_S00188509
+{
+    public class Phone
+    {
+        // Props
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+
+        // Methods
+
+        public void IncreasePrice(decimal amount)
+        {
+            Price = Price + (Price * amount);
+        }
+    }
+}
